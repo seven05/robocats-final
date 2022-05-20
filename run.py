e02816fee29a7094bddb29a4ab6460287d0cd7f3
crmin <crmin@superclass.io>
2022-05-20 21:49:28 +0900

    Make entry point

---
+++ run.py
@@ -0,0 +1,30 @@
+import argparse
+import importlib
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(description='Robotics final project')
+    parser.add_argument('task', help='task id, choose in (1, 2, 3, 4) (only number)')
+    args = parser.parse_args()
+
+    task_id = 0
+
+    if args.task in ('1', 't1', 'task1'):
+        task_id = 1
+    elif args.task in ('2', 't2', 'task2'):
+        task_id = 2
+    elif args.task in ('3', 't3', 'task3'):
+        task_id = 3
+    elif args.task in ('4', 't4', 'task4'):
+        task_id = 4
+    else:
+        print(f'Wrong format input: {args.task}')
+        print('Plz try again, This process will be exited.')
+        sys.exit(1)
+
+    print(f'Run task#{task_id}')
+    task_module = importlib.import_module(f't{task_id}')
+    task_module.main()
+
+if __name__ == '__main__':
+    main()
---
+++ t3.py
@@ -0,0 +1,5 @@
+def main():
+    pass  # TODO: Write entry code here
+
+if __name__ == '__main__':
+    main()
